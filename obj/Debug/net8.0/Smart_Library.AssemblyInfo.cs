f049762bb4a2a287d9b9a0a76c0870e4374cb22d
K-Joshua <saintangel9989@gmail.com>
2025-11-25 21:21:23 +0800

    Added Gitignore For Our Leila

--- obj/Debug/net8.0/Smart_Library.AssemblyInfo.cs
+++ obj/Debug/net8.0/Smart_Library.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Smart_Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f4b9eb324b7bafd30a88a2d0164c849edcad582")]
 [assembly: System.Reflection.AssemblyProductAttribute("Smart_Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Smart_Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
